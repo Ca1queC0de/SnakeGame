297dd3a4f0b05cace2409a76f632bbbfc3a0364e
Caíque <149615073+Ca1queC0de@users.noreply.github.com>
2024-08-22 19:54:16 -0300

    Update SnakeGame.java

--- src/com.snakegame/SnakeGame.java
+++ src/com.snakegame/SnakeGame.java
@@ -1,9 +1,16 @@
 package com.snakegame;
 
+import javax.swing.JFrame;
+
 public class SnakeGame {
 
 	public static void main(String[] args) {
-		System.out.println("Jogo inicializado!");
+		JFrame frame = new JFrame();
+        frame.setTitle("Jogo da Cobrinha");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(1600, 900); // Tamanho da janela
+        frame.setVisible(true);  // Torna a janela visível
+        frame.setLocationRelativeTo(null); // Centraliza a janela na tela
 	}
 	
 }
