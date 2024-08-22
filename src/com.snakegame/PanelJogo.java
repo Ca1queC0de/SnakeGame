2210eb1820be78a2d95d22e414fde9199d7d0343
Caíque <149615073+Ca1queC0de@users.noreply.github.com>
2024-08-22 20:05:32 -0300

    Update PanelJogo.java

--- src/com.snakegame/PanelJogo.java
+++ src/com.snakegame/PanelJogo.java
@@ -10,6 +10,8 @@ public class PanelJogo extends JPanel{
 
 	protected void paintComponent(java.awt.Graphics g) {
 		super.paintComponent(g);
+		g.setColor(java.awt.Color.BLUE); //define a cor do fundo da tela
+		g.fillRect(0, 0, getWidth(), getHeight()); //preenche com a cor selecionada
 	}
 	
 }
