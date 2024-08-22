531ec311b03be3eaad7c05bdeaf9ba3ae45830e4
Caíque <149615073+Ca1queC0de@users.noreply.github.com>
2024-08-22 19:58:41 -0300

    Create PanelJogo.java

---
+++ src/com.snakegame/PanelJogo.java
@@ -0,0 +1,15 @@
+package com.snakegame;
+
+import javax.swing.JPanel;
+
+public class PanelJogo extends JPanel{
+
+	public PanelJogo() {
+
+	}
+
+	protected void paintComponent(java.awt.Graphics g) {
+		super.paintComponent(g);
+	}
+	
+}
